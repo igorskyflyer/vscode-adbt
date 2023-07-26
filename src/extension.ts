4efed7d12873458282b0564021b6d1e2b6b46f8c
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-26 16:12:41 +0200

    Support comments and functions auto-complete

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,26 @@
 import * as vscode from 'vscode'
 import { getAriaFunctions } from './features/auto-complete/functions'
+import { tokenType } from './features/auto-complete/tokenType'
+import { getCommentModifiers } from './features/auto-complete/comments'
 
 function provideCompletionItems(
   document: vscode.TextDocument,
   position: vscode.Position
 ) {
-  return [...getAriaFunctions()]
+  const items: vscode.CompletionItem[] = []
+  const lineText: string = document.lineAt(position.line).text
+  const textBeforeCursor: string = lineText.substring(0, position.character)
+
+  if (
+    textBeforeCursor.startsWith(tokenType.internalComment) ||
+    textBeforeCursor.startsWith(tokenType.exportedComment)
+  ) {
+    items.push(...getCommentModifiers())
+  } else {
+    items.push(...getAriaFunctions())
+  }
+
+  return items
 }
 
 function activate(context: vscode.ExtensionContext) {
