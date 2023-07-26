92212e1805c33277215748922f0f7803bba9f6a6
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-26 16:46:20 +0200

    Add hover info provider

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,8 @@ import { getAriaFunctions } from './features/auto-complete/functions'
 import { tokenType } from './features/auto-complete/tokenType'
 import { getCommentModifiers } from './features/auto-complete/comment-modifiers'
 import { getComments } from './features/auto-complete/comments'
+import { HoverInfoText } from './features/hover-info/HoverInfoText'
+import { HoverToken } from './features/hover-info/HoverToken'
 
 function provideCompletionItems(
   document: vscode.TextDocument,
@@ -24,10 +26,34 @@ function provideCompletionItems(
   return items
 }
 
+function provideHover(
+  document: vscode.TextDocument,
+  position: vscode.Position
+) {
+  const wordRange: vscode.Range | undefined =
+    document.getWordRangeAtPosition(position)
+
+  if (!wordRange) {
+    return
+  }
+
+  const word: string = document.getText(wordRange)
+  let hoverText: string = 'No hover information available.'
+
+  if (word in HoverInfoText) {
+    hoverText = HoverInfoText[word as HoverToken]
+  }
+
+  return new vscode.Hover(hoverText)
+}
+
 function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.languages.registerCompletionItemProvider('adbt', {
       provideCompletionItems,
+    }),
+    vscode.languages.registerHoverProvider('adbt', {
+      provideHover,
     })
   )
 }
