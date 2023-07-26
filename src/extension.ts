385d2edad7c47e1cda4aa6a4c58eff7f228aafb4
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-26 17:14:52 +0200

    Externalize hover provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,6 @@
 import * as vscode from 'vscode'
 import { provideCompletionItems } from './features/auto-complete/autoCompleteProvider'
-import { HoverInfoText } from './features/hover-info/HoverInfoText'
-import { HoverToken } from './features/hover-info/HoverToken'
-
-function provideHover(
-  document: vscode.TextDocument,
-  position: vscode.Position
-) {
-  const wordRange: vscode.Range | undefined =
-    document.getWordRangeAtPosition(position)
-
-  if (!wordRange) {
-    return
-  }
-
-  const word: string = document.getText(wordRange)
-  let hoverText: string = ''
-
-  if (word in HoverInfoText) {
-    hoverText = HoverInfoText[word as HoverToken]
-  }
-
-  return new vscode.Hover(hoverText)
-}
+import { provideHover } from './features/hover-info/hoverProvider'
 
 function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
