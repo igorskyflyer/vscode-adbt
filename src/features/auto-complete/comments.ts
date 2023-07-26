d991cf12a83941fcd034813ec918860353f2b34b
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-26 16:23:55 +0200

    Add internal comment

--- src/features/auto-complete/comments.ts
+++ src/features/auto-complete/comments.ts
@@ -1,3 +1,13 @@
 import * as vscode from 'vscode'
 
-export function getComments(): vscode.CompletionItem[] {}
+export function getComments(): vscode.CompletionItem[] {
+  const itemInternal: vscode.CompletionItem = new vscode.CompletionItem(
+    '@',
+    vscode.CompletionItemKind.Snippet
+  )
+  itemInternal.detail = '@'
+  itemInternal.documentation = 'Inserts an internal comment.'
+  itemInternal.insertText = '@ '
+
+  return [itemInternal]
+}
