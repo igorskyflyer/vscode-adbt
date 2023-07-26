0b4c36a2f555daa6133873c4fa65ebb89eddb6bd
igorskyflyer <igor.dvlpr@gmail.com>
2023-07-26 16:27:25 +0200

    Update details for comments

--- src/features/auto-complete/comments.ts
+++ src/features/auto-complete/comments.ts
@@ -6,7 +6,8 @@ export function getComments(): vscode.CompletionItem[] {
     vscode.CompletionItemKind.Snippet
   )
   itemInternal.detail = '@'
-  itemInternal.documentation = 'Inserts an internal comment.'
+  itemInternal.documentation =
+    'Inserts an internal comment. After insertion, trigger auto-complete again to show comment modifiers, i.e. TODO, FIXME, NOTE.'
   itemInternal.insertText = '@ '
 
   const itemExported: vscode.CompletionItem = new vscode.CompletionItem(
@@ -14,7 +15,8 @@ export function getComments(): vscode.CompletionItem[] {
     vscode.CompletionItemKind.Snippet
   )
   itemExported.detail = '#'
-  itemExported.documentation = 'Inserts an exported comment.'
+  itemExported.documentation =
+    'Inserts an exported comment. After insertion, trigger auto-complete again to show comment modifiers, i.e. TODO, FIXME, NOTE.'
   itemExported.insertText = '# '
 
   return [itemInternal, itemExported]
