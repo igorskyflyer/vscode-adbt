1eefb9a93bc1665f6a0726c3fd205c8199c3a16b
igorskyflyer <igor.dvlpr@gmail.com>
2023-08-03 00:58:22 +0200

    Update directives

--- src/features/auto-complete/directives.ts
+++ src/features/auto-complete/directives.ts
@@ -7,6 +7,8 @@ export function getDirectives(): vscode.CompletionItem[] {
   )
   itemAdblockDirective.detail = '[Adblock Plus]'
   itemAdblockDirective.documentation = 'Inserts a header directive.'
+  // intentionally drop the first square bracket
+  // because '[' is set as as trigger character
   itemAdblockDirective.insertText = new vscode.SnippetString(
     'Adblock Plus ${1:2.0}]'
   )
