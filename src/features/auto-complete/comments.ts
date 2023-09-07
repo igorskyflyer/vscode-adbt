b03947338f1ecb28668f9e2449e6cbb703d99768
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-07 23:13:07 +0200

    Add ADBT info comment

--- src/features/auto-complete/comments.ts
+++ src/features/auto-complete/comments.ts
@@ -19,5 +19,16 @@ export function getComments(): vscode.CompletionItem[] {
     'Inserts an exported comment. After insertion, trigger auto-complete again to show comment modifiers, i.e. TODO, FIXME, NOTE.'
   itemExported.insertText = '# '
 
-  return [itemInternal, itemExported]
+  const itemAdbtInfo: vscode.CompletionItem = new vscode.CompletionItem(
+    'ADBT version',
+    vscode.CompletionItemKind.Snippet
+  )
+
+  itemAdbtInfo.detail = '@ [ADBT: x.x.x]'
+  itemAdbtInfo.documentation = new vscode.MarkdownString(
+    'Inserts current ADBT version being used.\n\n*Does **not** affect the compiler, use for informational purposes only.*'
+  )
+  itemAdbtInfo.insertText = new vscode.SnippetString('@ [ADBT: 2.0.0]')
+
+  return [itemInternal, itemExported, itemAdbtInfo]
 }
