2b3ee6729c9564477d429a36010efa104a47f71f
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-08 23:00:29 +0200

    Add support for compound statements

--- src/features/hover-info/HoverInfoText.ts
+++ src/features/hover-info/HoverInfoText.ts
@@ -1,16 +1,80 @@
-import { HoverToken } from './HoverToken'
+import * as vscode from 'vscode'
+import { HoverInfoEntry } from './HoverInfoEntry'
 
-export const HoverInfoText: Record<HoverToken, string> = {
-  include:
-    "Includes a filter file. If the path argument doesn't exist or is invalid, Aria will abort compilation.",
-  header:
-    "Includes a header file. If the path argument doesn't exist or is invalid, Aria will abort compilation.",
-  export:
-    "Exports all rules to the provided filepath. If the path argument doesn't exist or is invalid, Aria will abort compilation.",
-  nl: 'Inserts a blank newline (LF).',
-  '@': 'Inserts an internal comment. After insertion, trigger auto-complete again to show comment modifiers, i.e. TODO, FIXME, NOTE.',
-  '#': 'Inserts an exported comment. After insertion, trigger auto-complete again to show comment modifiers, i.e. TODO, FIXME, NOTE.',
-  import:
-    "Includes a filter file and prepends its file path as a comment. If the path argument doesn't exist or is invalid, Aria will abort compilation.",
-  tag: 'Includes a tag, special, auto-enumerated comment with an optional description. Useful for code navigation, searching, etc.',
+export function findToken(
+  word: string,
+  textBeforeCursor: string
+): vscode.Hover {
+  let found: boolean = false
+  let hover: string = 'No information available.'
+
+  word = word.trim().toLowerCase()
+  textBeforeCursor = textBeforeCursor.toLowerCase()
+
+  for (let i = 0; i < HoverInfoText.length; i++) {
+    if (found) {
+      break
+    }
+
+    const entry: HoverInfoEntry = HoverInfoText[i]
+    found = false
+
+    if (entry.token === '@' && textBeforeCursor.startsWith(entry.token)) {
+      hover = entry.info
+      found = true
+      break
+    }
+
+    if (entry.token === '#' && textBeforeCursor.startsWith(entry.token)) {
+      hover = entry.info
+      found = true
+      break
+    }
+
+    if (entry.token === word) {
+      hover = entry.info
+      found = true
+      break
+    }
+
+    if (entry.subtokens instanceof Array && entry.subtokens.length > 0) {
+      const count: number = entry.subtokens.length
+
+      for (let j = 0; j < count; j++) {
+        if (entry.subtokens[j].token === word) {
+          hover = entry.subtokens[j].info
+          found = true
+          break
+        }
+      }
+    }
+  }
+
+  return new vscode.Hover(hover)
 }
+
+const HoverInfoText: HoverInfoEntry[] = [
+  new HoverInfoEntry('include', 'Includes a filter file.'),
+  new HoverInfoEntry('header', 'Includes a header file.'),
+  new HoverInfoEntry('export', 'Exports all rules to the provided filepath.'),
+  new HoverInfoEntry('nl', 'Inserts a blank newline (LF).'),
+  new HoverInfoEntry('@', 'Inserts an internal comment.'),
+  new HoverInfoEntry('#', 'Inserts an exported comment.'),
+  new HoverInfoEntry(
+    'import',
+    'Includes a filter file and prepends its file path as a comment.'
+  ),
+  new HoverInfoEntry(
+    'tag',
+    'Includes a tag, special, auto-enumerated comment with an optional description.'
+  ),
+  new HoverInfoEntry(
+    'meta',
+    "Inlines metadata to use in the template's header.",
+    [
+      new HoverInfoEntry('title', 'Inlines title metadata to use.'),
+      new HoverInfoEntry('expires', 'Inlines expires metadata to use.'),
+      new HoverInfoEntry('description', 'Inlines description metadata to use.'),
+    ]
+  ),
+]
