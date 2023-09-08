4c4aa9b58e2a0ce3004e7300027a4be90517db10
igorskyflyer <igor.dvlpr@gmail.com>
2023-09-08 23:00:41 +0200

    Add support for compound statements and Strings

--- src/features/hover-info/hoverProvider.ts
+++ src/features/hover-info/hoverProvider.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode'
-import { HoverInfoText } from './HoverInfoText'
-import { HoverToken } from './HoverToken'
+import { findToken } from './HoverInfoText'
 
 export function provideHover(
   document: vscode.TextDocument,
@@ -8,17 +7,36 @@ export function provideHover(
 ): vscode.Hover | undefined {
   const wordRange: vscode.Range | undefined =
     document.getWordRangeAtPosition(position)
-  let hoverText: string = 'No information available.'
 
   if (!wordRange) {
-    return new vscode.Hover(hoverText)
+    return new vscode.Hover('No information available.')
+  }
+
+  const stringRange: vscode.Range | undefined = document.getWordRangeAtPosition(
+    position,
+    /'[^']*'/
+  )
+
+  if (stringRange) {
+    return new vscode.Hover('A UTF-8 encoded string.')
   }
 
   const word: string = document.getText(wordRange)
+  const currentLine: vscode.TextLine = document.lineAt(position.line)
 
-  if (word in HoverInfoText) {
-    hoverText = HoverInfoText[word as HoverToken]
+  if (position.character <= currentLine.range.end.character) {
+    const lineText: string = currentLine.text
+    const wordRange: vscode.Range | undefined =
+      document.getWordRangeAtPosition(position)
+
+    if (wordRange) {
+      const wordStart: number = wordRange.start.character
+      const textBeforeCursor: string = lineText.substring(0, wordStart).trim()
+      return findToken(word, textBeforeCursor)
+    }
+  } else {
+    return findToken(word, word)
   }
 
-  return new vscode.Hover(hoverText)
+  return new vscode.Hover('No information available.')
 }
